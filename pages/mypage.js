d75fb18a2501b41bfc267f7ea7d22c9f4012edc1
che-ri <rnjs1135@gmail.com>
2021-11-06 10:50:59 +0900

    [fix] build 오류 수정

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,37 +1,34 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import PostSaveBtn from "../src/Components/Mypage/PostSaveBtn";
 import CircleImage from "../src/Components/Shared/CircleImage";
 import Modal from "../src/Components/Shared/Modal";
 
-import { useDispatch, useSelector } from "react-redux";
-
+import { useSelector } from "react-redux";
 
 //마이페이지 첫 렌더링시 메이블린 api 가져와서 list에 저장하기
 const mypage = (props) => {
-    const dispatch = useDispatch();
-    const userProfile = useSelector((state) => state.user.user)
-    
+  const userProfile = useSelector((state) => state?.user);
 
-    return (
-        <Container>
-            <CircleImage />
-            <div>
-                <h1>{userProfile&&userProfile.userNickname}</h1>
-                <P>{userProfile&&userProfile.userEmail}</P>
-            </div>
-            <PostSaveBtn />
-            <Modal />
-        </Container>
-    );
+  return (
+    <Container>
+      <CircleImage />
+      <div>
+        <h1>{userProfile && userProfile.userNickname}</h1>
+        <P>{userProfile && userProfile.userEmail}</P>
+      </div>
+      <PostSaveBtn />
+      <Modal />
+    </Container>
+  );
 };
 
 const P = styled.p`
-    color: #B8B8B8;
-`
+  color: #b8b8b8;
+`;
 
 const Container = styled.div`
-    text-align: center;
-`
+  text-align: center;
+`;
 
 export default mypage;
--- src/Components/Mypage/PostSaveBtn.jsx
+++ src/Components/Mypage/PostSaveBtn.jsx
@@ -1,64 +1,54 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const PostSaveBtn = ({value, onClick}) => {
-
-  function handleTitleClick() {
-    
-  }
-
-
-const h1 = document.querySelector("div.hello:first-child h1");
-
-
-  return(
+const PostSaveBtn = ({ value, onClick }) => {
+  return (
     <>
       <Container>
         <div>
           <PostButton clickMyPostBtn={() => {}}>작성한 글</PostButton>
           <Line />
         </div>
-      
+
         <div>
           <SaveButton clickSavePostBtn={() => {}}>저장한 글</SaveButton>
           <Line />
         </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
 PostSaveBtn.defaultProps = {
-  value: '',
-  onClick: ()=>{}
+  value: "",
+  onClick: () => {}
 };
 
 const Container = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-`
+`;
 
 const PostButton = styled.button`
   width: 100px;
   padding: 12px 0px;
-  color: #FF7775;
+  color: #ff7775;
   cursor: pointer;
-`
+`;
 
 const SaveButton = styled.button`
   width: 100px;
   padding: 12px 0px;
-  color: #FF7775;
+  color: #ff7775;
   cursor: pointer;
-`
+`;
 
 const Line = styled.div`
   width: 100%;
   height: 3px;
-  background-color: #FF7775;
+  background-color: #ff7775;
   /* display: none; */
-`
-
+`;
 
 export default PostSaveBtn;
