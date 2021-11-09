ff927dc99bd0e7959cfdf69b2e3e1f80674c1bab
che-ri <rnjs1135@gmail.com>
2021-11-09 12:05:47 +0900

    [feat] PWA 셋팅

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true
+};
+
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: { dest: "public" }
+});
--- pages/search.js
+++ pages/search.js
@@ -10,6 +10,7 @@ import FormControl from "@mui/material/FormControl";
 import { BsSearch } from "react-icons/bs";
 import { BiX } from "react-icons/bi";
 import { searchApi } from "../src/Shared/api";
+import LogoEffect from "../src/Components/Shared/LogoEffect";
 
 export default function Search() {
   const [term, setTerm] = useState("");
@@ -74,6 +75,7 @@ export default function Search() {
             />
           </Tag>
         ))}
+        <LogoEffect />
       </TagWrapper>
     </div>
   );
