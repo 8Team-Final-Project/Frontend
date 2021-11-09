796154bbc4841bf31d14616915421a14796308b2
seol <rrrreeees894@gmail.com>
2021-11-09 10:24:26 +0900

    [fit] 로그아웃 기능 오류 수정

--- src/Redux/Modules/userSlice.js
+++ src/Redux/Modules/userSlice.js
@@ -2,8 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 import { 
     postSignup,
     postLogin,
-    postCheckEmail,
-    postCheckNic,
     postLogout,
     Me,
     patchUserid
@@ -29,32 +27,6 @@ const userSlice = createSlice({
             state.isFetching = true;
         },
         [postSignup.rejected]: (state, { payload: errorMessage }) => {
-            state.isFetching = false;
-            state.errorMessage = errorMessage;
-        },
-
-        // 이메일 중복 확인
-        [postCheckEmail.fulfilled]: (state, { payload }) => {
-            state.isFetching = false;
-            state.errorMessage = null;
-        },
-        [postCheckEmail.pending]: (state, { payload }) => {
-            state.isFetching = true;
-        },
-        [postCheckEmail.rejected]: (state, { payload: errorMessage }) => {
-            state.isFetching = false;
-            state.errorMessage = errorMessage;
-        },
-
-        // 닉네임 중복 확인
-        [postCheckNic.fulfilled]: (state, { payload }) => {
-            state.isFetching = false;
-            state.errorMessage = null;
-        },
-        [postCheckNic.pending]: (state, { payload }) => {
-            state.isFetching = true;
-        },
-        [postCheckNic.rejected]: (state, { payload: errorMessage }) => {
             state.isFetching = false;
             state.errorMessage = errorMessage;
         },
