8299dfca072495ec1a52a145f18a1f4bb22bc1bc
che-ri <rnjs1135@gmail.com>
2021-11-10 13:51:39 +0900

    [feat] 에러페이지, 로딩페이지 작성

---
+++ src/Components/Shared/Error.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { useRouter } from "next/dist/client/router";
+import refreshIcon from "../src/Asset/icons/refresh_icon.svg";
+
+export default function Error() {
+  const router = useRouter();
+  return (
+    <Container>
+      <MessageBox>
+        <MainMessage>오류가 발생하였습니다.</MainMessage>
+        <SubMessage>다시 접속해주세요!</SubMessage>
+      </MessageBox>
+      <Icon src={refreshIcon.src} onClick={() => router.push("/")} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+`;
+
+const Icon = styled.img`
+  width: 60px;
+  height: 55px;
+  cursor: pointer;
+`;
+
+const MessageBox = styled.div`
+  color: #3c3c3c;
+  margin-bottom: 55px;
+  text-align: center;
+  font-size: 24px;
+`;
+
+const MainMessage = styled.p`
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+const SubMessage = styled.span``;
