1a880b924fda3678789b1a1bfc16d91afa3bceaf
che-ri <rnjs1135@gmail.com>
2021-11-11 23:22:10 +0900

    [fix] state 오류 수정 & 다운로드완료 버튼 추가

--- src/Components/Shared/AppDownloadBtn.jsx
+++ src/Components/Shared/AppDownloadBtn.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import styled from "styled-components";
 
 //components
-import RedButton from "../Button/RedButton";
 import WhiteButton from "../Button/WhiteButton";
 import HoneyButton from "../Button/HoneyButton";
 
@@ -16,13 +14,13 @@ export default function AppDownloadBtn() {
       e.preventDefault();
       deferredPrompt.current = e;
     });
-
     //설치가 되어있다면 버튼은 숨긴다
-    if (!deferredPrompt.current) {
-      setBtnOn(false);
+    if (deferredPrompt.current) {
+      return setBtnOn(false);
     }
-    //버튼을 보여줌
-    setBtnOn(true);
+
+    //설치가 되어있지않다면 버튼을 보여준다.
+    if (!deferredPrompt) setBtnOn(true);
   }, []);
 
   const installApp = () => {
@@ -35,14 +33,15 @@ export default function AppDownloadBtn() {
     deferredPrompt.current.userChoice.then((choiceResult) => {
       //설치 했을 때
       if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-        setIsBtnOn(false);
+        setBtnOn(false);
       } else {
-        //설치 하지 않았을 때
-        console.log("User dismissed the A2HS prompt");
+        //설치를 하지 않았을 때
+        setBtnOn(true);
       }
     });
   };
 
-  return <>{btnOn && <HoneyButton onClick={installApp} value="다운로드" />}</>;
+  return (
+    <>{btnOn ? <HoneyButton onClick={installApp} value="다운로드" /> : <WhiteButton disable value="다운로드 완료" />}</>
+  );
 }
