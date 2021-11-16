e1b7e93754ada5435f07d65733d4f6b5c01b2854
Park Juseung <pjstab0322@gmail.com>
2021-11-16 16:26:34 +0900

    [fix] cors error test2

--- src/Shared/api.js
+++ src/Shared/api.js
@@ -16,6 +16,8 @@ instance.interceptors.request.use(async (config) => {
   //getToken는 로컬 스토리지에 토큰이 있다면 반환한다 없다면 null 값 반환
   config.headers["Authorization"] = await getToken();
   //CORS 설정
+  config.withCredentials = true;
+
 
 
   return config;
