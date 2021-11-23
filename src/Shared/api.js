c279c8576ff8835a75954cc28a4d99112a9f2e42
NomadSuin <dpfflsk45@gmail.com>
2021-11-23 17:26:38 +0900

    [fix] API 주소 변경

--- src/Shared/api.js
+++ src/Shared/api.js
@@ -4,7 +4,7 @@ import { getToken } from "./util";
 // Axios 인스턴스 설정
 const instance = axios.create({
   // 백엔드 배포 주소
-  baseURL: "http://54.180.137.99"
+  baseURL: "https://kkuljohab.shop"
 });
 
 //interceptor를 통한 header 설정
@@ -66,7 +66,7 @@ export const postApi = {
 
   // 꿀조합 게시글 삭제하기
   deletePost: (postId) => instance.delete(`/api/v1/post/postdelete/${postId}`),
-  
+
   // 꿀조합 상세포스트 불러오기
   getPost: (postId) => instance.get(`/api/v1/post/${postId}`),
 
@@ -95,7 +95,7 @@ export const commentApi = {
 
   // 댓글 삭제하기
   deleteComment: (commentId) => {
-    return instance.delete(`/api/v1/comment/commentdelete/${commentId}`)
+    return instance.delete(`/api/v1/comment/commentdelete/${commentId}`);
   }
 };
 
