0aa9e794b6e9622800ca77b2d31ff06a32cb8a8c
Park Juseung <pjstab0322@gmail.com>
2021-11-25 15:37:25 +0900

    [style] 오타수정

--- src/Components/Input/HashTagWriteInput.jsx
+++ src/Components/Input/HashTagWriteInput.jsx
@@ -25,7 +25,7 @@ export default function HashTagInput({ tagList, setTagList, important, label, pl
 
   const handleOnKeyUp = (e) => {
     const { code: keyValue } = e;
-    if (keyValue === "Enter") {
+    if (keyValue === "Space" || keyValue === KeyEvent.KEYCODE_SPACE) {
       //스페이스바를 누르면 term을 태그목록에 추가하도록 한다.
 
       //값이 없으면 추가하지 않는다.
