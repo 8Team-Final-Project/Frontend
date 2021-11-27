25788bdcaaa62e38f7f0a5f48e388a865f7ec1f8
Park Juseung <pjstab0322@gmail.com>
2021-11-27 15:44:42 +0900

    [chore] 서버 url 변경 ( 오류 확인용)

--- src/Shared/api.js
+++ src/Shared/api.js
@@ -4,7 +4,7 @@ import { getToken } from "./util";
 // Axios 인스턴스 설정
 const instance = axios.create({
   // 백엔드 배포 주소
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: 'https://kkuljohab.shop'
   // baseURL: process.env.NEXT_PUBLIC_TEST_URL
 });
 
@@ -114,7 +114,7 @@ export const uploadApi = {
       for (let entry of Object.entries(req)) {
         formData.append(entry[0], entry[1]);
       }
-      const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + "/api/v1/post/uploadimg", formData);
+      const response = await axios.post("https://kkuljohab.shop/api/v1/post/uploadimg", formData);
       if (response.statusText === "OK") return response;
     } catch (err) {
       alert(err);
